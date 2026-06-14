ea49afe9500477c24cfa0895eef97693eee3a372
VSSILPA <VSSILPA@users.noreply.github.com>
2026-06-14 10:51:31 +0100

    Add: longest-consecutive-sequence - submission-7

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-7.py
@@ -0,0 +1,12 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        num_dict = collections.Counter(nums)
+        max_length = 0
+        for num in nums:
+            cur_length = 0
+            while num + cur_length in num_dict:
+                cur_length = cur_length + 1
+                if cur_length > max_length:
+                    max_length = cur_length
+        return max_length
+    
